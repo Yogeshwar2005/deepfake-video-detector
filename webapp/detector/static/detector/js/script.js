d7f9dfe2447c56c1dde98327fee83bbba614a174
Chowonky <thisisyogeshwar@gmail.com>
2026-06-14 09:07:39 +0530

    add dark mode

--- webapp/detector/static/detector/js/script.js
+++ webapp/detector/static/detector/js/script.js
@@ -1,5 +1,28 @@
-document.getElementById("upload-form").addEventListener("submit", ()=>{
+uploadForm = document.getElementById("upload-form");
+
+if (uploadForm){
+    uploadForm.addEventListener("submit", ()=>{
     document.getElementById("spinner").style.display="block";
     document.getElementById("detect-btn").disabled = true;
-    document.getElementById("detect-btn").innerText="Detecting..."
-})
+    document.getElementById("detect-btn").innerText="Detecting...";
+});
+}
+
+const themeToggle = document.getElementById("theme-toggle");
+
+const savedTheme = localStorage.getItem("theme") || "light";
+document.documentElement.setAttribute("data-bs-theme", savedTheme);
+
+if (themeToggle){
+    themeToggle.innerText = savedTheme === "dark"? "Light Mode" : "Dark Mode";
+    themeToggle.addEventListener("click", ()=>{
+        const currentTheme = document.documentElement.getAttribute("data-bs-theme");
+        const newTheme = currentTheme === "dark"? "light": "dark";
+        
+        document.documentElement.setAttribute("data-bs-theme", newTheme);
+        localStorage.setItem("theme", newTheme);
+        themeToggle.innerText = newTheme ==="dark"? "Light Mode" : "Dark Mode";
+    });
+
+
+}
